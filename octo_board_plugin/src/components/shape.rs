e1712b418b877f76f337c15a9241ce490d0e2008
AntonMakesGames <antonmakesgames@gmail.com>
2022-11-27 03:11:55 +0100

    iterating on initialization

--- octo_board_plugin/src/components/shape.rs
+++ octo_board_plugin/src/components/shape.rs
@@ -1,8 +1,23 @@
 use bevy::prelude::*;
 
+use std::fmt::{
+    *,
+    Display
+};
+
 #[cfg_attr(feature = "debug", derive(bevy_inspector_egui::Inspectable))]
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Component)]
 pub enum Shape {
     Octo,
     Quad,
+}
+
+impl Display for Shape {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        
+        match self {
+            Shape::Quad=> write!(f, "Quad"),
+            Shape::Octo=> write!(f, "Octo"),
+        }
+    }
 }
--- octo_board_plugin/src/game_plugin.rs
+++ octo_board_plugin/src/game_plugin.rs
@@ -108,48 +108,71 @@ fn spawn_tiles(
     let tile_gap = tile_scale / 2.;
     let tile_map = TileMap::create(OCTO_ON_SIDE);
     for tile in tile_map.map {
-        match tile {
+        let (mesh, material, tile_transform, shape, coord) =  match tile {
             Tile::Quad(x, y) => {
-                let bundle = MaterialMesh2dBundle {
-                    mesh: meshes.add(create_quad(octo_ratio, tile_scale, gap).into()).into(),
-                    material: materials.add(ColorMaterial::from(Color::RED)),
-                    transform: Transform::from_xyz(x as f32 * tile_gap, y as f32* tile_gap, 0.0),
-                    ..default()};
-    
-                let coord = TileCoord{x : x as i32 , y: y as i32};
-                commands.spawn_bundle(bundle)
-                    .insert_bundle(PickableBundle::default())
-                    .insert(coord)
-                    .insert(Shape::Quad)
-                    .insert(Name::new(format!("Quad ({})", coord)));
+                (create_quad(octo_ratio, tile_scale, gap).into(),
+                ColorMaterial::from(Color::RED),
+                Transform::default().with_translation(Vec3::new(x as f32 * tile_gap, y as f32 * tile_gap, 0_f32)),
+                Shape::Quad,
+                TileCoord{x: x as i32,y: y as i32})
             },
-
             Tile::Octo(x, y) => {
-                let bundle = MaterialMesh2dBundle {
-                    mesh: meshes.add(create_octogone(octo_ratio, tile_scale, gap).into()).into(),
-                    material: materials.add(ColorMaterial::from(Color::GRAY)),
-                    transform: Transform::from_xyz(x as f32 * tile_gap, y as f32 * tile_gap, 0.0),
-                    ..default()};
-    
-                let coord = TileCoord{x : x as i32, y: y as i32};
-                commands.spawn_bundle(bundle)
-                    .insert_bundle(PickableBundle::default())
-                    .insert(coord)
-                    .insert(Shape::Octo)
-                    .insert(Name::new(format!("Octo ({})", coord)));
+                (create_octogone(octo_ratio, tile_scale, gap).into(),
+                ColorMaterial::from(Color::GRAY),
+                Transform::default().with_translation(Vec3::new(x as f32 * tile_gap, y as f32 * tile_gap, 0_f32)),
+                Shape::Octo,
+                TileCoord{x: x as i32, y: y as i32})
             }
-        }
+        };
+        
+        let bundle = MaterialMesh2dBundle {
+            mesh: meshes.add(mesh).into(),
+            material: materials.add(material),
+            transform: tile_transform,
+            ..default()};
+
+        commands.spawn_bundle(bundle)
+            .insert(shape)
+            .insert(coord)
+            .insert(Name::new(format!("{} ({})", shape, coord)));
     }
 }
 
-fn spawn_pawns(
+fn spawn_pawns (
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>)
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    tiles: Query<(&TileCoord)>)
 {
     commands.spawn().insert(Pawn::Top(Some(TileCoord{x: 3, y: 0}))).insert(Name::new("Top"));
     commands.spawn().insert(Pawn::Top(Some(TileCoord{x: 4, y: 1}))).insert(Name::new("Top"));
     commands.spawn().insert(Pawn::Top(Some(TileCoord{x: 5, y: 0}))).insert(Name::new("Top"));
+
+    spawn_pawn(commands, meshes, materials, Pawn::Top(Some(TileCoord {x: 3 , y: 1})), tiles);
+}
+
+fn spawn_pawn (
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    pawn: Pawn,
+    tiles: Query<(&TileCoord)>)
+{
+    let (coord, label) = match pawn {
+        Pawn::Top(Some(coord)) => (coord, "Top"),
+        Pawn::Bottom(Some(coord)) => (coord, "Bottom"),
+        _=> panic!()
+    };
+
+    commands.spawn_bundle(MaterialMesh2dBundle {
+            mesh: meshes.add(shape::RegularPolygon::new(1_f32, 19).into()).into(),
+            transform: Transform::default().with_translation(Vec3::new(0., 0., 1.)).with_scale(Vec3::splat(32.)),
+            material: materials.add(ColorMaterial::from(Color::BLUE)),
+            ..Default::default()
+        })
+        .insert(pawn)
+        .insert(Name::new(label));
+
 }
 
 #[derive(Default)]
@@ -173,8 +196,7 @@ pub fn input_system(mut events: EventReader<PickingEvent>, tiles: Query<(&TileCo
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_tiles)
-        .add_startup_system(spawn_pawns);
+        app.add_startup_system(spawn_tiles);
         
         app.add_plugin(PickingPlugin).add_plugin(InteractablePickingPlugin);
         app.add_system_to_stage(CoreStage::PostUpdate, input_system);
